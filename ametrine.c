dc56ad73e87ec1d196befef6239807579a074e6b
truta193 <trutaandrei703@yahoo.com>
2021-06-24 15:05:29 -0700

    Fixed window terminate event (WINDOWS)

--- ametrine.c
+++ ametrine.c
@@ -877,6 +877,7 @@ void am_platform_linux_event_handler(XEvent *xevent) {
             platform->callbacks.am_platform_mouse_motion_callback(xevent->xany.window, xevent->xbutton.x, xevent->xbutton.y, AM_EVENT_MOUSE_MOTION);
             break;
         };  
+        //TODO: (LINUX) Send message from am_platform_window_terminate & look at windows version for solution here
         case ClientMessage: {
             if (xevent->xclient.data.l[0] = XInternAtom(platform->display, "WM_DELETE_WINDOW", false)) {
                 bool check_last_root = false;
@@ -958,7 +959,21 @@ LRESULT CALLBACK am_platform_win32_event_handler(HWND handle, uint32 event, WPAR
             info->ptMinTrackSize.y = 1;
             break;
         };
-        //REVIEW: See what happens when X is pressed, might need to clean up memory
+        case WM_DESTROY: {
+            am_dyn_array_remove(&platform->windows, am_platform_window_index_lookup(window_handle), 1);
+            am_free(am_platform_window_index_lookup(window_handle));
+            
+            bool check_no_root = true;
+            for (int32 i = 0; i < platform->windows.length; i++) 
+                if (am_dyn_array_data_retrieve(&platform->windows, am_window*, i)->info.parent == AM_WINDOW_ROOT_PARENT) {
+                    check_no_root = false;
+                    break;
+                };
+
+            //HACK: Temporary var. to quit loop, should send proper closing signal
+            if (check_no_root) temp_check = false;
+            break;
+        };
         default: break;
     };
     return DefWindowProc(handle, event, wparam, lparam);
@@ -1231,16 +1246,17 @@ int32 am_platform_window_index_lookup(uint64 handle) {
     return -1;
 };
 
+//TODO: (LINUX) Send message to loop
 void am_platform_window_terminate(am_window *window) {
     am_platform *platform = am_engine_get_subsystem(platform);
     #if defined(AM_LINUX)
     XUnmapWindow(platform->display, (Window)window->handle);
     XDestroyWindow(platform->display, (Window)window->handle);
+    am_free(window);
+    am_dyn_array_remove(&platform->windows, am_platform_window_index_lookup(window->handle), 1); 
     #else
     DestroyWindow((HWND)window->handle);
     #endif
-    am_free(window);
-    am_dyn_array_remove(&platform->windows, am_platform_window_index_lookup(window->handle), 1);  
 };
 
 //----------------------------------------------------------------------------//
@@ -1260,7 +1276,7 @@ int main() {
         .is_fullscreen = false
     };
     am_window *wind = am_platform_window_create(test);
-    XSetWindowBackground(test_platform->display, wind->handle, 0x0000FF);
+    //XSetWindowBackground(test_platform->display, wind->handle, 0x0000FF);
 
     am_window_info test2 = {
         .window_height = 50,
@@ -1271,18 +1287,18 @@ int main() {
         .is_fullscreen = false
     };
     am_window *wind2 = am_platform_window_create(test2);
-    XSetWindowBackground(test_platform->display, wind2->handle, 0x00FF00);
+    //XSetWindowBackground(test_platform->display, wind2->handle, 0x00FF00);
     
     am_window_info test3 = {
-        .window_height = 20,
-        .window_width = 20,
+        .window_height = 100,
+        .window_width = 100,
         .window_title = "Win2",
-        .window_position = {10,10},
+        .window_position = {50,800},
         .parent = AM_WINDOW_ROOT_PARENT,
         .is_fullscreen = false
     };
     am_window *wind3 = am_platform_window_create(test3);
-    XSetWindowBackground(test_platform->display, wind3->handle, 0xFF0000);
+    //XSetWindowBackground(test_platform->display, wind3->handle, 0xFF0000);
 
 
     uint64 t = 0;
@@ -1301,6 +1317,7 @@ int main() {
         if (am_platform_mouse_wheel_delta() != 0) printf("Scrolled\n");
     };
 
+    printf("got here\n");
     am_platform_terminate(test_platform);
     getchar();
     printf("done\n");
